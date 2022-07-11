84ce4c3a12d95caa1ddfbdbf323deab967c33342
EdvaldoUFRN <edvaldo_junior2009@hotmail.com>
2022-07-11 11:29:49 -0300

    Xd

--- campominado.c
+++ campominado.c
@@ -8,7 +8,7 @@ int main(void) {
   char campo[linha][coluna];
   //revelar todas as células sem revelar uma das minas.
 
-
+//TESTE EDVALDO
 
   //Uma mina é revelada: nesse caso, jogo encerra com a derrota do usuário;
 
